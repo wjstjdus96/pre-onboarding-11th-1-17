80610858a6e18ba43b838136541a896880e85202
seoyeon <wjstjdus96@naver.com>
2023-06-29 02:14:50 +0900

    feat(App): BrowerRouter을 통한 라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div></div>;
-}
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Todo from "./components/Todo";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Route>
+        <Route>
+          <Route path="/todo" element={<Todo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
