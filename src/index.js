c47dab32035ecb19cf40b3d8941f312de7447c93
seoyeon <wjstjdus96@naver.com>
2023-06-29 02:05:00 +0900

    feat(index) : globalstyle 지정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+*{
+    box-sizing: border-box;
+ }
+
+    html, body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        
+        width: 100%;
+        height: 100%;
+        
+        button:hover{
+            cursor: pointer;
+        }
+
+        #root {
+            height: 100%;
+        }
+    }
+  
+`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
