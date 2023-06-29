98fc0570da930610c728bfcea7071945547afea2
wjstjdus96 <wjstjdus96@naver.com>
2023-06-29 14:45:51 +0900

    fix(SignUp): 상수처리

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,43 @@
+import AuthForm from "../components/authForm/AuthForm";
 import AuthLayout from "../layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import { requestSignUp } from "../apis/auth";
+import { useAuthForm } from "../hooks/useAuthForm";
+import { checkValidation } from "../utils/checkValidation";
+import { SIGN_UP } from "../constants/const";
 
 export default function SignUp() {
-  return <AuthLayout>회원가입</AuthLayout>;
+  const navigate = useNavigate();
+
+  const {
+    authFormValue: { email, password },
+    handleChange,
+  } = useAuthForm();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await requestSignUp(email, password)
+      .then((res) => {
+        if (res?.status === 201) {
+          navigate("/signin");
+        }
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  return (
+    <AuthLayout>
+      <AuthForm
+        title={SIGN_UP}
+        buttonTestId={"signup-button"}
+        buttonName={SIGN_UP}
+        onSubmit={handleSubmit}
+        onInputChange={handleChange}
+        isDisabledButton={checkValidation(email, password)}
+      />
+    </AuthLayout>
+  );
 }
